4ab064ef7b16516bd8db15b8ce04b55e675e5105
Hà Phạm Anh Huy _084205005137 <huyhpa5137@ut.edu.vn>
2025-11-07 21:05:53 +0700

    add

--- src/tracker/vehicle_tracker.py
+++ src/tracker/vehicle_tracker.py
@@ -1,5 +1,4 @@
 import numpy as np
-from collections import defaultdict
 from ..models.vehicle import Vehicle
 
 class VehicleTracker:
@@ -8,159 +7,93 @@ class VehicleTracker:
         self.vehicles = {}
         self.next_id = 1
         self.frame_count = 0
-        
+
     def calculate_iou(self, box1, box2):
-        """Calculate IoU between two bounding boxes"""
-        x1_1, y1_1, x2_1, y2_1 = box1
-        x1_2, y1_2, x2_2, y2_2 = box2
-        
-        # Calculate intersection area
-        x1_inter = max(x1_1, x1_2)
-        y1_inter = max(y1_1, y1_2)
-        x2_inter = min(x2_1, x2_2)
-        y2_inter = min(y2_1, y2_2)
-        
-        if x2_inter <= x1_inter or y2_inter <= y1_inter:
+        """Tính IoU giữa hai bounding box"""
+        x1, y1 = max(box1[0], box2[0]), max(box1[1], box2[1])
+        x2, y2 = min(box1[2], box2[2]), min(box1[3], box2[3])
+        if x2 <= x1 or y2 <= y1:
             return 0
-        
-        inter_area = (x2_inter - x1_inter) * (y2_inter - y1_inter)
-        box1_area = (x2_1 - x1_1) * (y2_1 - y1_1)
-        box2_area = (x2_2 - x1_2) * (y2_2 - y1_2)
-        union_area = box1_area + box2_area - inter_area
-        
-        return inter_area / union_area if union_area > 0 else 0
-        
-    def assign_vehicle_ids(self, current_detections):
-        """Assign IDs to detected vehicles"""
+        inter = (x2 - x1) * (y2 - y1)
+        area1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
+        area2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
+        return inter / (area1 + area2 - inter + 1e-6)
+
+    def assign_vehicle_ids(self, detections):
+        """Gán ID cho các phát hiện"""
         if not self.vehicles:
-            # No existing tracks, assign new IDs to all
-            assigned_detections = {}
-            for cls, det in current_detections:
-                assigned_detections[self.next_id] = (cls, det)
-                self.next_id += 1
-            return assigned_detections
-            
-        # Create IoU matrix
-        iou_matrix = np.zeros((len(current_detections), len(self.vehicles)))
-        for i, (_, det) in enumerate(current_detections):
-            for j, (vehicle_id, vehicle) in enumerate(self.vehicles.items()):
-                iou_matrix[i, j] = self.calculate_iou(det['box'], vehicle.last_box)
-                
-        # Assign IDs based on IoU
-        assigned_detections = {}
-        used_tracks = set()
-        used_detections = set()
-        
-        # Find best matches (highest IoU)
+            assigned = {self.next_id + i: det for i, det in enumerate(detections)}
+            self.next_id += len(detections)
+            return assigned
+
+        iou_matrix = np.array([[self.calculate_iou(det[1]['box'], v.last_box)
+                               for v in self.vehicles.values()] for det in detections])
+
+        assigned, used_det, used_track = {}, set(), set()
         while True:
-            max_iou = 0
-            best_i, best_j = -1, -1
-            
-            for i in range(len(current_detections)):
-                if i in used_detections:
-                    continue
-                for j in range(len(self.vehicles)):
-                    if j in used_tracks:
-                        continue
-                    if iou_matrix[i, j] > max_iou:
-                        max_iou = iou_matrix[i, j]
-                        best_i, best_j = i, j
-                        
-            # Stop if no IoU > threshold
+            max_idx = np.unravel_index(np.argmax(iou_matrix), iou_matrix.shape)
+            max_iou = iou_matrix[max_idx]
             if max_iou < self.config.IOU_THRESHOLD:
                 break
-                
-            # Assign ID
-            track_id = list(self.vehicles.keys())[best_j]
-            assigned_detections[track_id] = current_detections[best_i]
-            used_tracks.add(best_j)
-            used_detections.add(best_i)
-            
-        # Assign new IDs to unassigned detections
-        for i, (cls, det) in enumerate(current_detections):
-            if i not in used_detections:
-                assigned_detections[self.next_id] = (cls, det)
+            i, j = max_idx
+            vid = list(self.vehicles.keys())[j]
+            assigned[vid] = detections[i]
+            used_det.add(i)
+            used_track.add(j)
+            iou_matrix[i, :] = -1
+            iou_matrix[:, j] = -1
+
+        # Gán ID mới cho phát hiện chưa dùng
+        for i, det in enumerate(detections):
+            if i not in used_det:
+                assigned[self.next_id] = det
                 self.next_id += 1
-                
-        return assigned_detections
-        
-    def update(self, current_detections):
-        """Update vehicle tracking with new detections"""
+        return assigned
+
+    def update(self, detections):
+        """Cập nhật theo khung hình mới"""
         self.frame_count += 1
-        
-        # Assign IDs to current detections
-        assigned_detections = self.assign_vehicle_ids(current_detections)
-        
-        # Update existing vehicles and create new ones
-        for vehicle_id, (cls, detection) in assigned_detections.items():
-            if vehicle_id not in self.vehicles:
-                # Create new vehicle
-                self.vehicles[vehicle_id] = Vehicle(
-                    vehicle_id, cls, detection['box'], 
-                    detection['center'], detection['confidence'], 
-                    self.frame_count
-                )
+        assigned = self.assign_vehicle_ids(detections)
+
+        for vid, (cls, det) in assigned.items():
+            if vid not in self.vehicles:
+                self.vehicles[vid] = Vehicle(vid, cls, det['box'], det['center'], det['confidence'], self.frame_count)
             else:
-                # Update existing vehicle
-                vehicle = self.vehicles[vehicle_id]
-                vehicle.update(
-                    detection['box'], detection['center'], 
-                    detection['confidence'], self.frame_count
-                )
-                vehicle.calculate_movement(self.config)
-                
-        # Handle occlusion
+                v = self.vehicles[vid]
+                v.update(det['box'], det['center'], det['confidence'], self.frame_count)
+                v.calculate_movement(self.config)
+
         self._handle_occlusion()
-        
-        # Clean up old vehicles
         self._cleanup_vehicles()
-        
-        return assigned_detections
-        
+        return assigned
+
     def _handle_occlusion(self):
-        """Handle vehicle occlusion"""
-        for vehicle in self.vehicles.values():
-            frames_since_last_seen = self.frame_count - vehicle.last_seen_frame
-            
-            # Mark as occluded if not seen for a while
-            if frames_since_last_seen > 3 and not vehicle.is_occluded:
-                vehicle.freeze_status()
-                
-            # Update frozen status frames
-            if vehicle.is_occluded:
-                vehicle.frozen_status_frames += 1
-                
+        """Xử lý xe bị che khuất"""
+        for v in self.vehicles.values():
+            if self.frame_count - v.last_seen_frame > 3:
+                if not v.is_occluded:
+                    v.freeze_status()
+                v.frozen_status_frames += 1
+
     def _cleanup_vehicles(self):
-        """Remove old and low-confidence vehicles"""
-        to_remove = []
-        for vehicle_id, vehicle in self.vehicles.items():
-            if vehicle.should_remove(self.frame_count, self.config):
-                to_remove.append(vehicle_id)
-                
-        for vehicle_id in to_remove:
-            del self.vehicles[vehicle_id]
-            
+        """Xóa xe cũ hoặc kém tin cậy"""
+        self.vehicles = {vid: v for vid, v in self.vehicles.items()
+                         if not v.should_remove(self.frame_count, self.config)}
+
     def get_statistics(self):
-        """Get tracking statistics"""
-        car_count = sum(1 for v in self.vehicles.values() if v.vehicle_class == 2)
-        motorbike_count = sum(1 for v in self.vehicles.values() if v.vehicle_class == 3)
-        
-        moving_count = 0
-        stopped_count = 0
-        for vehicle in self.vehicles.values():
-            effective_status = vehicle.get_effective_status()
-            if effective_status == 'stopped' and vehicle.status_frames >= self.config.MIN_FRAMES_STOP:
-                stopped_count += 1
-            else:
-                moving_count += 1
-                
-        occluded_count = sum(1 for v in self.vehicles.values() if v.is_occluded)
-        
+        """Thống kê trạng thái xe"""
+        cars = sum(v.vehicle_class == 2 for v in self.vehicles.values())
+        bikes = sum(v.vehicle_class == 3 for v in self.vehicles.values())
+        stopped = sum(v.get_effective_status() == 'stopped' and v.status_frames >= self.config.MIN_FRAMES_STOP
+                      for v in self.vehicles.values())
+        moving = len(self.vehicles) - stopped
+        occluded = sum(v.is_occluded for v in self.vehicles.values())
+
         return {
             'total': len(self.vehicles),
-            'cars': car_count,
-            'motorbikes': motorbike_count,
-            'moving': moving_count,
-            'stopped': stopped_count,
-            'occluded': occluded_count
+            'cars': cars,
+            'motorbikes': bikes,
+            'moving': moving,
+            'stopped': stopped,
+            'occluded': occluded
         }
